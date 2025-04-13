1719f8682b5c258d03b7e5d64570b6e3fddb4d8c
Sergey Rudenko <rudenko.s@gmail.com>
2025-04-13 20:04:25 +0400

    pages

--- src/main/kotlin/Driver.kt
+++ src/main/kotlin/Driver.kt
@@ -1,10 +1,14 @@
 package my.jdbc.wsdl_driver
 
-import java.sql.*
+import java.sql.DriverManager
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+private val wsdlLogger: Logger = LoggerFactory.getLogger("my.jdbc.wsdl_driver")
 
 fun registerDriver() {
     // The WsdlDriver will parse the connection string (URL) to extract wsdlEndpoint and reportPath.
     val driver = WsdlDriver()
     DriverManager.registerDriver(driver)
-    println("WSDL JDBC Driver registered successfully.")
+    wsdlLogger.info("WSDL JDBC Driver registered successfully.")
 }
